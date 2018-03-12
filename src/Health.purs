1f1b6f8bff147ba64946aa5c066ddb5c2638e0aa
Philippe Vinchon <pvinchon@gilt.com>
2018-03-12 17:59:41 +0000

    PureScript mapping for aws-sdk-js 2.207.0

--- src/Health.purs
+++ src/Health.purs
@@ -20,37 +20,47 @@ import Data.StrMap as StrMap
 import AWS.Request as Request
 import AWS.Request.Types as Types
 
-serviceName = "Health" :: String
-
 
 -- | <p>Returns a list of entities that have been affected by the specified events, based on the specified filter criteria. Entities can refer to individual customer resources, groups of customer resources, or any other construct, depending on the AWS service. Events that have impact beyond that of the affected entities, or where the extent of impact is unknown, include at least one entity indicating this.</p> <p>At least one event ARN is required. Results are sorted by the <code>lastUpdatedTime</code> of the entity, starting with the most recent.</p>
 describeAffectedEntities :: forall eff. DescribeAffectedEntitiesRequest -> Aff (exception :: EXCEPTION | eff) DescribeAffectedEntitiesResponse
-describeAffectedEntities = Request.request serviceName "describeAffectedEntities" 
+describeAffectedEntities = Request.request service method  where
+    service = Request.ServiceName "Health"
+    method = Request.MethodName "describeAffectedEntities"
 
 
 -- | <p>Returns the number of entities that are affected by each of the specified events. If no events are specified, the counts of all affected entities are returned.</p>
 describeEntityAggregates :: forall eff. DescribeEntityAggregatesRequest -> Aff (exception :: EXCEPTION | eff) DescribeEntityAggregatesResponse
-describeEntityAggregates = Request.request serviceName "describeEntityAggregates" 
+describeEntityAggregates = Request.request service method  where
+    service = Request.ServiceName "Health"
+    method = Request.MethodName "describeEntityAggregates"
 
 
 -- | <p>Returns the number of events of each event type (issue, scheduled change, and account notification). If no filter is specified, the counts of all events in each category are returned.</p>
 describeEventAggregates :: forall eff. DescribeEventAggregatesRequest -> Aff (exception :: EXCEPTION | eff) DescribeEventAggregatesResponse
-describeEventAggregates = Request.request serviceName "describeEventAggregates" 
+describeEventAggregates = Request.request service method  where
+    service = Request.ServiceName "Health"
+    method = Request.MethodName "describeEventAggregates"
 
 
 -- | <p>Returns detailed information about one or more specified events. Information includes standard event data (region, service, etc., as returned by <a>DescribeEvents</a>), a detailed event description, and possible additional metadata that depends upon the nature of the event. Affected entities are not included; to retrieve those, use the <a>DescribeAffectedEntities</a> operation.</p> <p>If a specified event cannot be retrieved, an error message is returned for that event.</p>
 describeEventDetails :: forall eff. DescribeEventDetailsRequest -> Aff (exception :: EXCEPTION | eff) DescribeEventDetailsResponse
-describeEventDetails = Request.request serviceName "describeEventDetails" 
+describeEventDetails = Request.request service method  where
+    service = Request.ServiceName "Health"
+    method = Request.MethodName "describeEventDetails"
 
 
 -- | <p>Returns the event types that meet the specified filter criteria. If no filter criteria are specified, all event types are returned, in no particular order.</p>
 describeEventTypes :: forall eff. DescribeEventTypesRequest -> Aff (exception :: EXCEPTION | eff) DescribeEventTypesResponse
-describeEventTypes = Request.request serviceName "describeEventTypes" 
+describeEventTypes = Request.request service method  where
+    service = Request.ServiceName "Health"
+    method = Request.MethodName "describeEventTypes"
 
 
 -- | <p>Returns information about events that meet the specified filter criteria. Events are returned in a summary form and do not include the detailed description, any additional metadata that depends on the event type, or any affected resources. To retrieve that information, use the <a>DescribeEventDetails</a> and <a>DescribeAffectedEntities</a> operations.</p> <p>If no filter criteria are specified, all events are returned. Results are sorted by <code>lastModifiedTime</code>, starting with the most recent.</p>
 describeEvents :: forall eff. DescribeEventsRequest -> Aff (exception :: EXCEPTION | eff) DescribeEventsResponse
-describeEvents = Request.request serviceName "describeEvents" 
+describeEvents = Request.request service method  where
+    service = Request.ServiceName "Health"
+    method = Request.MethodName "describeEvents"
 
 
 -- | <p>Information about an entity that is affected by a Health event.</p>
